72261ec5d70d1e36baa9079f06f629f3895002ff
Muhammad Shahzad <shahzadthathal@gmail.com>
2016-03-27 15:37:10 +0500

    new changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -70,7 +70,8 @@ var clientApp = angular.module('clientApp', [
         redirectTo: '/'
       });
 
-    //$locationProvider.html5Mode(true);  
+    // use the HTML5 History API
+      //$locationProvider.html5Mode(true);
      
   });
 
